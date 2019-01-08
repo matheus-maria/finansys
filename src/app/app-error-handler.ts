dea927cb18cba5138aaed4fd14cdcf6c623517e2
Matheus Maria <mmatheus.maria@gmail.com>
2019-01-08 20:04:03 -0200

    Category Service

---
+++ src/app/app-error-handler.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export class ErrorHandler {
+    
+    static handleError(error: HttpErrorResponse | any){
+        let errorMessage: string
+
+        if(error instanceof HttpErrorResponse){
+            errorMessage = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
+        }
+        else{
+            errorMessage = error.toString();
+        }
+
+        console.log(errorMessage);
+        return Observable.throw(errorMessage)
+    }    
+}
---
+++ src/app/pages/categories/category.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+import { map, catchError, flatMap } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from './category.model';
+import { ErrorHandler } from 'src/app/app-error-handler';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  private apiPath: string = "api/categories"
+
+  constructor(private http: HttpClient, private error) { }
+
+  categories = (): Observable<Category[]> => {
+    return this.http.get<Category[]>(this.apiPath).pipe( 
+      catchError(ErrorHandler.handleError)
+    )
+  }
+
+  categoryById = (id: number): Observable<Category> => {
+    return this.http.get<Category>(`${this.apiPath}/${id}`).pipe( 
+      catchError(ErrorHandler.handleError)
+    )
+  }
+
+  create = (category: Category): Observable<number> => {
+    return this.http.post<Category>(this.apiPath,category).pipe(
+      map(category => category.id),
+      catchError(ErrorHandler.handleError)
+    )
+  }
+
+  update = (id: number): Observable<number> => {
+    return this.http.put<Category>(`${this.apiPath}/${id}`, id).pipe(
+      map(() => id),
+      catchError(ErrorHandler.handleError)
+    )
+  }
+
+  delete = (id: number): Observable<number> => {
+    return this.http.delete<Category>(`${this.apiPath}/${id}`).pipe(
+      map(() => id),
+      catchError(ErrorHandler.handleError)
+    )
+  }
+
+}
