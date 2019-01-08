1f788beed69bfa0a6aeb8e5504844b87d4d6c5e1
Matheus Maria <mmatheus.maria@gmail.com>
2019-01-08 17:50:28 -0200

    Categories

---
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fns-category-form',
+  templateUrl: './category-form.component.html',
+  styleUrls: ['./category-form.component.sass']
+})
+export class CategoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
